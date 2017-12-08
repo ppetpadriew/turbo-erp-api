4289be94a4bd697bfa6e38804ab1a47e96a5f721
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2017-12-08 20:59:24 +0700

    feat(Providers)

---
+++ src/Providers/BroadcastServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Turbo\ERP\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Broadcast;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Broadcast::routes();
+
+        require base_path('routes/channels.php');
+    }
+}
