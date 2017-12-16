6754f50c881e471596dfe855e70b47dffd418ec8
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2017-12-16 22:23:20 +0700

    style(type hint)

--- database/migrations/Migration.php
+++ database/migrations/Migration.php
@@ -3,10 +3,12 @@
 namespace App\Database\Migrations;
 
 use App\Constants\Service;
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Support\Facades\Facade;
 
 abstract class Migration extends \Illuminate\Database\Migrations\Migration
 {
+    /** @var ConnectionInterface */
     protected $db;
 
     public function __construct()
