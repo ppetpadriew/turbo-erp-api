a894cae14c529c287f2629bffa8aa07abc589f0c
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2017-12-27 16:33:24 +0700

    chore: register new generator in app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Wn\Generators\CommandsServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() === 'local') {
+            $this->app->register(CommandsServiceProvider::class);
+        }
     }
 }
