8ca7431008d1351c368de60a639a60fbe9ff06ca
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2018-01-01 11:55:14 +0700

    feat(Controller): add base routing to base controller class.
    Also our class to BaseController to solve class name collision. This will make auto complete in PHPStorm work again.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Laravel\Lumen\Routing\Controller as BaseController;
+use App\Models\Model;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class Controller extends BaseController
+class BaseController extends Controller
 {
-    //
+    /** @var string */
+    protected $modelClass;
+
+    public function index()
+    {
+        return $this->modelClass::all();
+    }
+
+    public function create(Request $request)
+    {
+        return $this->modelClass::create($request->toArray());
+    }
+
+    public function update(int $id, Request $request)
+    {
+        /** @var Model $record */
+        $record =  $this->modelClass::find($id);
+
+        if(empty($record)) {
+            throw new HttpException(404, 'Record not found.');
+        }
+
+        $record->fill($request->toArray());
+
+        $record->save();
+
+        return $record;
+    }
+
+    public function delete(int $id)
+    {
+        /** @var Model $record */
+        $record =  $this->modelClass::find($id);
+
+        if(empty($record)) {
+            throw new HttpException(404, 'Record not found.');
+        }
+
+        $record->delete();
+
+        return $record;
+    }
 }
