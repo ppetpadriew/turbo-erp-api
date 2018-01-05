41b29c33c2a6f5ae4fb85114aad70fc9fb7de611
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2018-01-06 01:13:29 +0700

    test: introduce some tests to prove it works

---
+++ database/seeds/UnitControllerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Database\Seeds;
+
+
+use App\Models\Unit;
+
+class UnitControllerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->db->table(Unit::TABLE)->delete();
+        $this->db->table(Unit::TABLE)->insert([
+            ['id' => 1, 'code' => 'un1', 'description' => 'un1 desc'],
+            ['id' => 2, 'code' => 'un2', 'description' => 'un2 desc'],
+        ]);
+    }
+}
