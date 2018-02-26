c6efac6b9aed717126cebfca2869adbd230044c6
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2018-02-26 22:35:18 +0700

    feat(Model): add scenario based validations.
    This makes the system more flexible to handle validation based on insert, update or any custom business scenarios.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 
-class Model extends \Illuminate\Database\Eloquent\Model
+use ReflectionClass;
+
+abstract class Model extends \Illuminate\Database\Eloquent\Model
 {
+    const SCENARIO_CREATE = 'create';
+    const SCENARIO_UPDATE = 'update';
+
     /**
      * Indicates if the model should be timestamped.
      *
@@ -12,6 +17,55 @@ class Model extends \Illuminate\Database\Eloquent\Model
      */
     public $timestamps = false;
 
+    protected $scenario = self::SCENARIO_CREATE;
+
+    private $scenarios = [];
+
     const CREATED_AT = 'created_datetime';
     const UPDATED_AT = 'updated_datetime';
+
+    public function setScenario(string $scenario): void
+    {
+        if (!$this->isValidScenario($scenario)) {
+            throw new \InvalidArgumentException("Scenario '{$scenario}' is not implemented yet.");
+        }
+
+        $this->scenario = $scenario;
+    }
+
+    public function isValidScenario(string $scenario): bool
+    {
+        return in_array($scenario, $this->getScenarios());
+    }
+
+    public function getScenarios(): array
+    {
+        if ($this->scenarios) {
+            return $this->scenarios;
+        }
+
+        $constants = (new ReflectionClass(get_class($this)))->getConstants();
+        $this->scenarios = array_filter($constants, function ($constant) {
+            return strpos($constant, 'SCENARIO_') !== false;
+        }, ARRAY_FILTER_USE_KEY);
+
+        return $this->scenarios;
+    }
+
+    public function fillable(array $fillable)
+    {
+        throw new \Exception('We do not allow to set fillable dynamically.');
+    }
+
+    public function newInstance($attributes = [], $exists = false)
+    {
+        $model = parent::newInstance($attributes, $exists);
+        $scenario = $model->exists
+            ? self::SCENARIO_UPDATE
+            : self::SCENARIO_CREATE;
+
+        $model->setScenario($scenario);
+
+        return $model;
+    }
 }
