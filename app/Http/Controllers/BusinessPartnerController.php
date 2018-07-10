fafcdc195d5ae69605c521f249beedef6dd5f1e7
Peeratchai Petpadriew <p.petpadriew@gmail.com>
2018-07-10 20:52:27 +0700

    wip

---
+++ app/Http/Controllers/BusinessPartnerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\BusinessPartner;
+
+class BusinessPartnerController extends BaseController
+{
+    public function getModelClass(): string
+    {
+        return BusinessPartner::class;
+    }
+}
